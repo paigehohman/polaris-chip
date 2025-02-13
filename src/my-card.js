d597cd9ab9f51499ae6e5a6ba9bad378b3ea0428
paigehohman <pjh5761@psu.edu>
2025-02-13 15:44:16 -0500

    Update my-card.js
    Homework 5

--- src/my-card.js
+++ src/my-card.js
@@ -15,12 +15,22 @@ export class MyCard extends LitElement {
     super();
     this.title = "my-card";
     this.img = "";
-    this.bgColor = "#000000";
     this.description = "";
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
+      :host {
+        display: inline-block;
+      }
+
+      :host([fancy]) .card {
+        display: block;
+        background-color: blue;
+        color: white;
+        box-shadow: 10px 5px 5px black;
+      }
       h1 {
         color: white;
         font-size: 40px;
@@ -34,18 +44,23 @@ export class MyCard extends LitElement {
       }
 
       img {
-        max-width: 300px;
-        margin-left: 50px;
+        height: 400px;
+        width: 400px;
+        margin: auto;
       }
 
       .card {
-        background-color: black;
-        width: 400px;
+        background-color: gray;
+        max-width: 400px;
+        height: 625px;
         padding: 20px;
-        margin: auto;
+        margin: 10px;
         border: solid yellow;
         border-width: 10px;
         border-radius: 10px;
+        display: inline-block;
+        position: relative;
+        overflow: hidden;
       }
 
       button {
@@ -81,18 +96,51 @@ export class MyCard extends LitElement {
 
       @media (max-width: 500px) {
         .card {
-          width: 100px;
+          width: 100%;
           margin: 10px;
+          height: auto;
           padding: 10px 30px 20px 10px;
         }
+
         img {
-          width: 100%;
-          padding: 10px;
+          max-width: 400px;
+          height: 200px;
+          align-items: center;
+          margin: auto;
+        }
+
+        h1 {
+          color: white;
+          font-size: 40px;
+          margin: auto;
+        }
+
+        p {
+          color: white;
+          font-size: 18px;
+          margin: auto;
         }
       }
 
-      :host {
-        display: block;
+      details summary {
+        text-align: left;
+        font-size: 20px;
+        padding: 8px 0;
+      }
+
+      details[open] summary {
+        font-weight: bold;
+      }
+
+      details div {
+        border: 2px solid black;
+        text-align: left;
+        padding: 8px;
+        height: 70px;
+        overflow-y: auto;
+        max-height: 100px;
+        position: auto;
+        bottom: 10px;
       }
     `;
   }
@@ -100,25 +148,38 @@ export class MyCard extends LitElement {
   render() {
     return html`
     <div class="card">
+
       <h1>${this.title}</h1>
       <img src=${this.img}>
-      <p>${this.description}</p>
+  <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+  <summary>Description</summary>
+  <div>
+    <slot>${this.description}</slot>
+  </div>
+</details>
       <button class="details_button">
         <a href="https://hax.psu.edu" >Details>
       </button>
         </div>
-  
-      
-      
       </div>`;
+  }
+
+  // put this anywhere on the MyCard class; just above render() is probably good
+  openChanged(e) {
+    console.log(e);
+    if (e.target.getAttribute("open") !== null) {
+      this.fancy = true;
+    } else {
+      this.fancy = false;
+    }
   }
 
   static get properties() {
     return {
+      fancy: { type: Boolean, reflect: true },
       title: { type: String },
       img: { type: String },
       description: { type: String },
-      bgColor: { type: String },
     };
   }
 }
